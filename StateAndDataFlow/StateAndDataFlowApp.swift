c972df6aa5041d6bf84da0b037c861bd53ce41c8
Hadro0n <v.w.yakunin@gmail.com>
2025-03-19 16:00:20 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Владислав Якунин on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
